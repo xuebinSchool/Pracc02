4f5d7e84f61e014636b68694e9b4264cd40d4839
xuebin <xuebinsng@gmail.com>
2020-11-03 15:16:29 +0800

    Finished Part 1?

---
+++ BusinessLogicLayer/ProductBLL.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DataAccessLayer;
+
+
+namespace BusinessLogicLayer
+{
+    public class ProductBLL
+    {
+        public Product GetProdDetail(decimal prodID)
+        {
+            Product prodDetail = new Product();
+
+            return prodDetail.GetProduct(prodID);
+        }
+        public List<Product> GetAllProduct()
+        {
+            List<Product> allProduct = new List<Product>();
+            Product product = new Product();
+
+            allProduct = product.GetProductAll();
+
+            return allProduct;
+        }
+    }
+}
---
+++ BusnissLogicLayer/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace BusnissLogicLayer
+{
+    public class Class1
+    {
+    }
+}
